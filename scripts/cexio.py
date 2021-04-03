081250157cdee86abc0aa14f71bd1508d802b660
Cosmin Poieana <cmin764@gmail.com>
2021-04-03 18:00:15 +0300

    Better scanner

--- scripts/cexio.py
+++ scripts/cexio.py
@@ -2,6 +2,7 @@
 
 
 import datetime
+import functools
 import itertools
 import json
 import operator
@@ -19,7 +20,9 @@ DATE_YESTERDAY = (
 URL_OHLCV = "https://cex.io/api/ohlcv/hd/{date}/{symbol1}/{symbol2}"
 
 DEFAULT_PAIRS = ["USD", "BTC", "ETH"]
-MIN_WIN = 0.01  # x% minimum win threshold
+FIAT = ["USD"]
+DIMS = [[1], [4], [2, 3]]
+MIN_WIN = 0.03  # x% minimum win threshold
 
 
 def _get_ohlcv(date, pair, is_reversed=False):
@@ -38,63 +41,98 @@ def _get_ohlcv(date, pair, is_reversed=False):
 
 
 def download_history(start=DATE_YESTERDAY, days=1, pairs=DEFAULT_PAIRS):
-    start_date = datetime.datetime.strptime(start, DATE_TEMPLATE)
+    start_date = datetime.datetime.strptime(str(start), DATE_TEMPLATE)
     for diff in range(days):
         crt_date = start_date + datetime.timedelta(days=diff)
         for pair in itertools.combinations(pairs, 2):
             pair, data = _get_ohlcv(crt_date, sorted(pair))
+            date_str = crt_date.strftime(DATE_TEMPLATE)
             fname = "{}_{}_{}.json".format(
-                crt_date.strftime(DATE_TEMPLATE), pair[0], pair[1]
+                date_str, pair[0], pair[1]
             )
             with open(fname, "w") as stream:
+                print(f"Dumping on {date_str} {pair[0]}/{pair[1]}: {len(data)} timestamps")
                 json.dump(data, stream)
 
 
 def _get_factor(ohlcv, dimensions):
-    assert 0 <= len(dimensions) < 2
+    assert 0 < len(dimensions) <= 2
     if len(dimensions) == 1:
         return ohlcv[dimensions[0]]
 
     return sum(ohlcv[dim] for dim in dimensions) / len(dimensions)
 
 
-def find_margins(cur_chain, crt_date, dimensions=[1]):
-    cur_chain = cur_chain.split()
+@functools.cache
+def _open_pair(crt_date, first, second, reverse=False):
+    fname = f"{crt_date}_{first}_{second}.json"
+    print(f"Trying to open {fname}...")
+    if os.path.isfile(fname):
+        with open(fname) as stream:
+            data = json.load(stream)
+            min_date = data[0][0]
+            max_date = data[-1][0]
+            data = {tick[0]: tick for tick in data}
+            return {"reverse": reverse, "data": data, "min_date": min_date, "max_date": max_date}
+
+    if reverse is True:
+        raise Exception("pair {first}/{second} not found at all")
+    return _open_pair(crt_date, second, first, reverse=True)
+
+
+def find_margins(cur_chain, crt_date_str, min_win=MIN_WIN):
+    print(f"Configuration: {crt_date_str} {cur_chain}")
+    if not isinstance(cur_chain, list):
+        cur_chain = cur_chain.split()
     assert len(cur_chain) > 3
     assert cur_chain[0] == cur_chain[-1]
-    data_chain = []
-    for idx in range(len(cur_chain) - 1):
-        reverse = False
-        for curs in itertools.permutations(cur_chain[idx:idx+2], 2):
-            fname = f"{crt_date}_{curs[0]}_{curs[1]}.json"
-            print(f"Trying to open {fname}...")
-            if os.path.isfile(fname):
-                with open(fname) as stream:
-                    data = json.load(stream)
-                    data_chain.append({"reverse": reverse, "data": data})
-                    break
-            else:
-                reverse = True
+    tran_chain = []
+    for time_idx in range(len(cur_chain) - 1):
+        tran_chain.append(_open_pair(crt_date_str, *cur_chain[time_idx:time_idx + 2]))
 
-    max_time = max([item["data"][0][0] for item in data_chain])
-    for data_item in data_chain:
-        while data_item["data"][0][0] < max_time:
-            data_item["data"].pop(0)
-    assert len(set([item["data"][0][0] for item in data_chain])) == 1
+    start_time = max([item["data"]["min_date"] for item in tran_chain])
+    end_time = min([item["data"]["max_date"] for item in tran_chain])
+    max_ts = min([len(item["data"]) for item in tran_chain])
 
-    min_length = min([len(item["data"]) for item in data_chain])
     wins = []
-    for idx in range(min_length):
-        win = 1.0
-        for data_item in data_chain:
-            oper = operator.truediv if data_item["reverse"] else operator.mul
-            factor = _get_factor(data_item["data"][idx], dimensions)
-            win = oper(win, factor)
-        if win - 1.0 > MIN_WIN:
-            wins.append((data_item["data"][idx][0], win))
+    ts = 0
+    for time_idx in range(start_time, end_time + 60, 60):
+        win_by_dims = {tuple(dims): 1.0 for dims in DIMS}
+        for tran_item in tran_chain:
+            tick = tran_item["data"].get(time_idx)
+            if not tick:
+                break
+            oper = operator.truediv if tran_item["reverse"] else operator.mul
+            for dims in win_by_dims:
+                factor = _get_factor(tick, dims)
+                win_by_dims[dims] = oper(win_by_dims[dims], factor)
+        else:
+            ts += 1
+            max_win_dims = max(win_by_dims, key=lambda k: win_by_dims[k])
+            max_win = win_by_dims[max_win_dims]
+            if max_win - 1.0 > min_win:
+                wins.append((time_idx, max_win, max_win_dims))
+
+    print("Timestamps/potential: ", ts, max_ts)
     print("Wins: ", wins)
     if wins:
         print("Max win: ", max(wins, key=lambda win: win[1]))
+
+
+def find_all_margins(start=DATE_YESTERDAY, days=1, pairs=DEFAULT_PAIRS):
+    start_date = datetime.datetime.strptime(str(start), DATE_TEMPLATE)
+    for diff in range(days):
+        crt_date = start_date + datetime.timedelta(days=diff)
+        crt_date_str = crt_date.strftime(DATE_TEMPLATE)
+        for begin in pairs:
+            if begin not in FIAT:
+                continue
+
+            crypto = set(pairs) - {begin}
+            for length in range(2, len(pairs)):
+                for inner_tran in itertools.permutations(crypto, length):
+                    cur_chain = [begin] + list(inner_tran) + [begin]
+                    find_margins(cur_chain, crt_date_str)
 
 
 if __name__ == "__main__":
